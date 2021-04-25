8e046948cc8bbe0e32782e622644a2d045197741
Steve Dubrov <ugurbado@gmail.com>
2021-04-25 23:15:48 +0700

    Update example.cpp
    broke clang format

--- test/example.cpp
+++ test/example.cpp
@@ -1,10 +1,11 @@
 #include "pch.h"
 #include "sum.h"
 
-TEST(Example, OnePlusOne) {
+TEST(Example, OnePlusOne) 
+{
     ASSERT_EQ(1 + 1, 2);
 }
 
 TEST(ExampleSum, OnePlusOne) {
-    ASSERT_EQ(sum(1, 2), 3);
+        ASSERT_EQ(sum(1, 2), 3);
 }
