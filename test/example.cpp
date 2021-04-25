b31fbfb370cbb02e9a370c759b0258e29236ce06
Steve Dubrov <ugurbado@gmail.com>
2021-04-26 01:48:45 +0700

    Update example.cpp

--- test/example.cpp
+++ test/example.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "sum.h"
 
-TEST(Example, OnePlusOne) { ASSERT_EQ(1 + 1, 2); }
+TEST(Example, OnePlusOne) { 
+  ASSERT_EQ(1 + 1, 2); }
 
 TEST(ExampleSum, OnePlusOne) { ASSERT_EQ(sum(1, 2), 3); }
